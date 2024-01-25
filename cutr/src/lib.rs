085e890be4e7293fa24985870d548a18f5cd1cac
Daniel Kukula <daniel.kuku@gmail.com>
2024-01-25 20:17:03 +0000

    parser skeleton

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -1,9 +1,10 @@
 use clap::{arg, command, Parser};
 use nom::{
     branch::alt,
+    bytes::complete::tag,
     character::complete::{char, one_of},
-    combinator::{opt, recognize},
-    multi::{many0, many1},
+    combinator::recognize,
+    multi::{many0, many1, separated_list0},
     sequence::{terminated, tuple},
     IResult,
 };
@@ -28,13 +29,13 @@ pub struct Config {
     #[arg(short, long = "delim", default_value = "\t", value_parser = parse_delimiter)]
     delimiter: u8,
     /// Selected characters
-    #[arg(short, long, default_value = "-", allow_hyphen_values(true), value_parser = parse_position)]
+    #[arg(short, long, default_value = "", allow_hyphen_values(true), value_parser = parse_position)]
     chars: PositionList,
     /// Selected bytes
-    #[arg(short, long, default_value = "-", allow_hyphen_values(true),  value_parser = parse_position)]
+    #[arg(short, long, default_value = "", allow_hyphen_values(true),  value_parser = parse_position)]
     bytes: PositionList,
     /// Selected fields
-    #[arg(short, long, default_value = "-", allow_hyphen_values(true), value_parser = parse_position)]
+    #[arg(short, long, default_value = "", allow_hyphen_values(true), value_parser = parse_position)]
     field: PositionList,
 }
 pub fn run(config: Config) -> CutResult<()> {
@@ -51,15 +52,32 @@ fn parse_delimiter(delim: &str) -> Result<u8, String> {
         .ok_or(String::from("Invalid delimiter"))
 }
 fn parse_position(input: &str) -> Result<PositionList, String> {
-    let _ = match range_input(input) {
-        Ok(("", result)) if result.starts_with("-") => Ok(vec![0..2]),
-        Ok(("", result)) if result.ends_with("-") => Ok(vec![1..2]),
-        Ok(("", result)) if result.contains("-") => Ok(vec![1..2]),
-        Ok(("", _int)) => Ok(vec![1..2]),
-        Ok((rest, _result)) => Err(format!("can't parse: {}", rest)),
-        Err(_e) => Err("Invalid range".to_string()),
-    };
-    todo!("use lexer to get the values")
+    let inputs =
+        match separated_list0(tag(","), range_input)(input).map_err(|_| "error".to_string()) {
+            Ok(("", inputs)) => inputs,
+            Ok((result, inputs)) => {
+                println!("{:?} {:?}", result, inputs);
+                unimplemented!()
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                unimplemented!()
+            }
+        };
+    let result = inputs
+        .iter()
+        .map(|r| match r.split("-").collect::<Vec<_>>()[..] {
+            [result] => {
+                let pos = result.parse().unwrap();
+                (pos - 1)..pos
+            }
+            ["", to] => 0..to.parse().unwrap(),
+            [from, ""] => from.parse().unwrap()..255,
+            [from, to] => from.parse().unwrap()..to.parse().unwrap(),
+            _ => unimplemented!(),
+        })
+        .collect::<Vec<Range<usize>>>();
+    Ok(result)
 }
 fn range_input(input: &str) -> IResult<&str, &str> {
     alt((
@@ -87,65 +105,92 @@ mod unit_tests {
     fn test_parse_position() {
         // The empty string is an error
         assert!(parse_position("").is_err());
-
+    }
+    #[test]
+    fn test_parse_position0() {
         // Zero is an error
         let res = parse_position("0");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"0\"",);
-
+    }
+    #[test]
+    fn test_parse_position01() {
         let res = parse_position("0-1");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"0\"",);
-
+    }
+    #[test]
+    fn test_parse_position_plus1() {
         // A leading "+" is an error
         let res = parse_position("+1");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"+1\"",);
-
+    }
+    #[test]
+    fn test_parse_position_plus12() {
         let res = parse_position("+1-2");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"+1-2\"",);
-
+    }
+    #[test]
+    fn test_parse_position_1plus2() {
         let res = parse_position("1-+2");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"1-+2\"",);
-
+    }
+    #[test]
+    fn test_parse_position_non_number() {
         // Any non-number is an error
         let res = parse_position("a");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a\"",);
-
+    }
+    #[test]
+    fn test_parse_position_number_non_number() {
         let res = parse_position("1,a");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a\"",);
-
+    }
+    #[test]
+    fn test_parse_position_number_non_number_range() {
         let res = parse_position("1-a");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"1-a\"",);
-
+    }
+    #[test]
+    fn test_parse_position_non_number_number_range() {
         let res = parse_position("a-1");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a-1\"",);
-
+    }
+    #[test]
+    fn test_parse_position_empty_range() {
         // Wonky ranges
         let res = parse_position("-");
         assert!(res.is_err());
-
+    }
+    #[test]
+    fn test_parse_position_comma() {
         let res = parse_position(",");
         assert!(res.is_err());
-
+    }
+    #[test]
+    fn test_parse_position_nothing_after_comma() {
         let res = parse_position("1,");
         assert!(res.is_err());
-
-        let res = parse_position("1-");
-        assert!(res.is_err());
-
+    }
+    #[test]
+    fn test_parse_position_invalid_range_int() {
         let res = parse_position("1-1-1");
         assert!(res.is_err());
-
+    }
+    #[test]
+    fn test_parse_position_invalid_range_char() {
         let res = parse_position("1-1-a");
         assert!(res.is_err());
-
+    }
+    #[test]
+    fn test_parse_position_invalid_range_same() {
         // First number must be less than second
         let res = parse_position("1-1");
         assert!(res.is_err());
@@ -153,14 +198,18 @@ mod unit_tests {
             res.unwrap_err().to_string(),
             "First number in range (1) must be lower than second number (1)"
         );
-
+    }
+    #[test]
+    fn test_parse_position_invalid_range_reverse() {
         let res = parse_position("2-1");
         assert!(res.is_err());
         assert_eq!(
             res.unwrap_err().to_string(),
             "First number in range (2) must be lower than second number (1)"
         );
-
+    }
+    #[test]
+    fn test_parse_position_ok() {
         // All the following are acceptable
         let res = parse_position("1");
         assert!(res.is_ok());
